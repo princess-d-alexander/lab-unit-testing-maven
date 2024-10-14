c9e2c0f6a328d0a244ac734172a0feac4d4517a1
Alexander <alexande2@russell.cs.grinnell.edu>
2024-10-14 17:47:52 -0500

    added some tests

--- src/test/java/edu/grinnell/csc207/util/TestSampleMethods.java
+++ src/test/java/edu/grinnell/csc207/util/TestSampleMethods.java
@@ -25,5 +25,33 @@ public class TestSampleMethods {
   //   assertEquals(2, 3);
   // } // sampleFailingTest()
 
+
+  // Test for Celsius to Fahrenheit conversion
+  @Test
+  public void testC2F() {
+      assertEquals(32, SampleMethods.c2f(0), "Celcius to Farenheit conversion at 0° C");
+      assertEquals(212, SampleMethods.c2f(100), "Celcius to Farenheit conversion at 100°C");
+  }
+
+  // Test for sum method
+  @Test
+  public void testSum() {
+      int[] values = {1, 2, 3};
+      assertEquals(6, SampleMethods.sum(values), "Sum of {1, 2, 3} should be 6");
+
+      int[] largeValues = {Integer.MAX_VALUE - 1, 1};
+      assertEquals(Integer.MAX_VALUE, SampleMethods.sum(largeValues), "Sum of extreme values");
+  }
+
+  // Test for extreme sums to ensure integer overflow is handled correctly
+  @Test
+  public void testExtremes() {
+      int tmp = Integer.MAX_VALUE - 10;
+      int[] values = {tmp, tmp, -tmp, -tmp};
+      // This test checks for integer overflow
+      assertEquals(0, SampleMethods.sum(values), "Sum of extreme values");
+    }
+
 } // class TestSampleMethods
+
 
